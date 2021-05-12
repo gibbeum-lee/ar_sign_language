e853dbb8bc67089f39e5b32552285752d5d20f8d
Gibbeum Lee <gbblee@kaist.ac.kr>
2021-05-12 21:43:14 +0900

    Add Temp model (for testing...)

--- Python/capture.py
+++ Python/capture.py
@@ -3,57 +3,75 @@
 
 import cv2
 import numpy as np
+from timer import Timer
 import torch
 
 MODEL_FILE = "slrecog.onnx"
 
 win_width = 1024
 win_height = 768
+fps = 30
 key_delay = 33 # ms
 
-# Open WebCam
-try:
-    camindex = 0 # 0: system default camera, or input a video file path
-    capture = cv2.VideoCapture(camindex) # ! wsl1 or 2 cannot use camera driver ... ㅠㅠ
-except:
-    print("No Camera Source Found")
+def video_start (device = 0, win_resolution = (win_width, win_height), fps = fps):
+    # device = 0: system default camera, or input a video file path
+    
+    # Open WebCam
+    try:
+        capture = cv2.VideoCapture(device) # ! wsl1 or 2 cannot use camera driver ... ㅠㅠ
+    except:
+        print("No Camera Source Found")
 
-# Use WebCam Frames
-while capture.isOpened():
-    print("Camera Opened")
+    if not capture.isOpened():
+        print("No Camera Opened...")
+    else:
+        # subwindow settings
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, win_width)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, win_height)
+        capture.set(cv2.CAP_PROP_FPS, fps)
+        print("Camera Opened and Initialized")
 
-    # subwindow settings
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, win_width)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, win_height)
+    return capture
 
-    while cv2.waitKey(key_delay) < 0:
-        # capture frames from the camera
-        retval, frame = capture.read() # frame is numpy.ndarray format
-        if not retval:
-            break # when it failed to get a new frame
+def video_capture ():
+    timer = Timer()
 
-        # show frames on the subwindow
-        cv2.imshow(winname = "WebCam", mat = frame)
-        print(frame)
+    # Start camera
+    stream = video_start()
 
-        # ====================== USING THE MODEL ==========================
-        # model input here
+    # Use WebCam Frames
+    while stream.isOpened():
 
+        while cv2.waitKey(key_delay) < 0:
+            # capture frames from the camera
+            retval, frame = stream.read() # frame is numpy.ndarray format
+            if not retval:
+                break # when it failed to get a new frame
 
+            # show frames on the subwindow
+            cv2.imshow(winname = "WebCam", mat = frame)
+            print(frame)
 
-        # model prediction here
-        #net = cv2.dnn.readNet(MODEL_FILE)
-
-
-
-        # model output here like text
-        word = '테스트'
+            # ====================== USING THE MODEL ==========================
+            # model input here
 
 
 
-        # =================================================================
+            # model prediction here
+            #net = cv2.dnn.readNet(MODEL_FILE)
 
-# Exit
-print("Camera Closed")
-capture.release()
-cv2.destroyAllWindows()
+
+
+            # model output here like text
+            word = '테스트'
+
+
+
+            # =================================================================
+
+    # Exit
+    print("Camera Closed")
+    stream.release()
+    cv2.destroyAllWindows()
+
+video_capture()
